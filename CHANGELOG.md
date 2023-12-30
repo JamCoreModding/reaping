ab76d60be4df9db72f4fec93ec25db6b7ee317bd
James (Jamalam) <james@jamalam.tech>
2023-12-30 19:51:18 +0000

    feat: 1.20.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,1 @@
-- Support NeoForge.
-- The Reaping pillager should now spawn in patrols.
-
-1.20.4 support will come soon.
+1.20.4 support.
--- forge/src/main/java/io/github/jamalam360/reaping/forge/ReapingPlatformImpl.java
+++ forge/src/main/java/io/github/jamalam360/reaping/forge/ReapingPlatformImpl.java
@@ -1,10 +1,9 @@
 package io.github.jamalam360.reaping.forge;
 
 import net.minecraft.world.entity.LivingEntity;
-import virtuoel.pehkui.api.ScaleTypes;
 
 public class ReapingPlatformImpl {
 	public static void setScale(LivingEntity entity, float scale) {
-		ScaleTypes.BASE.getScaleData(entity).setScale(scale);
+		// No-op, Pehkui no longer supports Forge
 	}
 }
